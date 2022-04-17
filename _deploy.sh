73b53375d110d76ccc5591127f6c2583aa061f3e
Peter Binkley <peter.binkley@ualberta.ca>
2022-04-17 15:44:25 -0600

    Fix upload of .wqhtaccess

--- _deploy.sh
+++ _deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 JEKYLL_ENV=production bundle exec jekyll b
-rsync -e ssh -a --delete _site/* wallandbinkley:./versdiff.wallandbinkley.com/
+rsync -e ssh -a _site/ wallandbinkley:./versdiff.wallandbinkley.com/ --delete
 
