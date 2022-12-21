0da8c2bbacc7788807eeadad9f12daba23fca767
Peter Binkley <peter.binkley@ualberta.ca>
2022-12-20 21:28:17 -0700

    Handle homonyms based on final word

--- _diacritics.rb
+++ _diacritics.rb
@@ -3,6 +3,12 @@
 require 'csv'
 require 'byebug'
 
+BREVE = 0
+MACRON = 1
+
+def diacritify(word)
+	# find second-last syllable
+end
 data = CSV.read('_data/versdiff.csv', headers: true)
 
 conversions = {
@@ -21,6 +27,8 @@ conversions = {
 }
 
 data.each do |verse|
+	next if verse['text'] != verse['textdiacritics']
+
 	text = verse['text']
 	next if text.include?('/') # skip multiple-line verses
 
@@ -33,8 +41,39 @@ data.each do |verse|
 	prevs = []
 	i = 0
 	while (i < last) do
-		prevs << tokens[i] if tokens[i] == tokens[last]
+		prevs << i if tokens[i] == tokens[last]
 		i += 1	
 	end
-	byebug
+	next if prevs.count != 1
+
+	term = tokens[last]
+	syllables = term.split(/([^aeiouyAEIOUY]+[aeiouyAEIOUY])/).reject { |c| c.empty? }
+	penult = (syllables.last.match?(/.*[aeiouyAEIOUY]$/)) ? syllables.count - 2 : syllables.count - 3
+	vowel = syllables[penult].scan(/[aeiouyAEIOUY]/).first
+	original_syllable = syllables[penult].dup
+
+	syllables[penult] = original_syllable.sub(vowel, conversions[vowel][BREVE])
+	tokens[prevs.first] = syllables.join
+	syllables[penult] = original_syllable.sub(vowel, conversions[vowel][MACRON])
+	tokens[last] = syllables.join
+
+	verse['textdiacritics'] = tokens.join
+end
+
+changed = 0
+unchanged = 0
+data.each do |verse|
+	if verse['text'] == verse['textdiacritics']
+		unchanged += 1
+	else
+		changed += 1
+	end
+end
+puts "Unchanged: #{unchanged}; changed: #{changed}"
+
+CSV.open("newdata.csv", "w") do |csv|
+  csv << data.headers
+  data.each do |verse|
+    csv << verse
+  end
 end
